dfefd4c76314cb5fda5192de0c4eca750f4d765d
Naun Rahimli <naunrehimli99@gmail.com>
2026-03-15 20:51:03 +0400

    Add files via upload

---
+++ Şifrə generatoru 1.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+print("Şifrə generatoru")
+
+uzunluq = int(input("Şifrə neçə simvol olsun?"))
+
+simvollar = string.ascii_letters + string.digits + string.punctuation
+
+sifre = ""
+
+for i in range(uzunluq):
+    sifre = sifre + random.choice(simvollar)
+    
+print("Şifrəniz:", sifre)
