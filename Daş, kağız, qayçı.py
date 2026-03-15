f9a015a62472f8e64f192d6e534789c18c0689c6
Naun Rahimli <naunrehimli99@gmail.com>
2026-03-15 20:22:55 +0400

    Add files via upload

---
+++ Daş, kağız, qayçı.py
@@ -0,0 +1,22 @@
+import random
+
+seçimlər = ["daş", "kağız", "qayçı"]
+
+print("Oyuna xoş gəldin!")
+
+kompüter = random.choice(seçimlər)
+oyunçu = input("Seçimini yaz (daş / kağız / qayçı): ")
+
+print("Sən seçdin:", oyunçu)
+print("Kompüter seçdi:", kompüter)
+
+if oyunçu == kompüter :
+    print("Bərabərə!")
+elif oyunçu == "daş" and kompüter == "qayçı":
+    print("Sən qazan!")
+elif oyunçu == "kağız" and kompüter == "daş":
+    print("Sən qazan!")
+elif oyunçu == "qayçı" and kompüter == "kağız":
+    print("Sən qazan!")
+else:
+    print("Kompüter qazandı!")
